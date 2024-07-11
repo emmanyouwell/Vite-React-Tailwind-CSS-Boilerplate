fffc836a17539f7866a34120d64531bec814e54e
emmanyouwell <emingala02@gmail.com>
2024-07-11 10:30:18 +0800

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React,{ useState } from "react";
+
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center">Vite React + Tailwind CSS Boilerplate</h1>
+    </>
+  );
+}
+
+export default App;
